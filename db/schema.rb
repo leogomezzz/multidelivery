a159b6ad0fd5845880cc86a5a4582448f5253360
leogomezzz <leonardo.delfica@gmail.com>
2014-02-17 18:17:20 -0300

    criacao do banco de teste categoria

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217205851) do
+ActiveRecord::Schema.define(version: 20140217211131) do
+
+  create_table "categoria", force: true do |t|
+    t.string   "nome"
+    t.string   "descricao"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "clientes", force: true do |t|
     t.string   "nome"
